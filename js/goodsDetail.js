84bf335fe9ef448ca84c3a15a64fdef42be84917
shangyou <317742988@qq.com>
2019-03-02 17:16:10 +0800

    这次增加了商品详情页面（动态切换商品图片）

--- js/goodsDetail.js
+++ js/goodsDetail.js
@@ -48,3 +48,83 @@ $(function(){
         }
     );
 });
+$(function(){
+    var mySwiper = new Swiper ('.swiper1', {
+        direction : 'horizontal',
+        centeredSlides: true,
+        pagination: {
+            el: '.swp1',
+            clickable: true,
+        }
+    })
+    var mySwiper2 = new Swiper ('.swiper2', {
+        direction : 'horizontal',
+        centeredSlides: true,
+        pagination: {
+            el: '.swp2',
+            clickable: true,
+        }
+    })
+    var mySwiper3 = new Swiper ('.swiper3', {
+        direction : 'horizontal',
+        centeredSlides: true,
+        pagination: {
+            el: '.swp3',
+            clickable: true,
+        }
+    })
+    var mySwiper4 = new Swiper ('.swiper4', {
+        direction : 'horizontal',
+        centeredSlides: true,
+        pagination: {
+            el: '.swp4',
+            clickable: true,
+        }
+    })
+    $(".desc_goods_list li").click(function(event){
+        if(event.currentTarget === this){
+            $(".desc_goods_list li").css({"border":"1px solid #fff"});
+            $(event.currentTarget).css({"border":"1px solid black"});
+            let dds=$(".con_goods .goods_list");
+            // console.log(dds);
+            let currIndex=$(this).attr("index");
+            // console.log(currIndex);
+            for(let i=0;i<dds.length;i++){
+                dds[i].style.display="none";
+            }
+            dds[currIndex].style.display="block";
+            // let imgs=$(".list_img dd:visible img");
+            // // console.log(imgs.length);
+            // for(let i=0;i<imgs.length;i++){
+            //     imgs[i].style.zIndex=1;
+            //     // console.log(imgs[i]);
+            // }
+            // imgs[0].style.zIndex=2;
+            // let uls=$(".diandian a");
+            // for(let i=0;i<uls.length;i++){
+            //     uls[i].style.backgroundColor="white";
+            // }
+            // uls[0].style.backgroundColor="gray";
+            // let h3s=$(".xq_right_tit h3");
+            // for(let i=0;i<h3s.length;i++){
+            //     h3s[i].style.display="none";
+            // }
+            // h3s[currIndex].style.display="block";
+            // let ps=$(".jingpian p");
+            // for(let i=0;i<ps.length;i++){
+            //     ps[i].style.display="none";
+            // }
+            // ps[currIndex].style.display="block";
+            // let spans=$(".rmb span");
+            // for(let i=0;i<spans.length;i++){
+            //     spans[i].style.display="none";
+            // }
+            // spans[currIndex].style.display="inline-block";
+            // let pss=$(".xq_xx2_pian");
+            // for(let i=0;i<pss.length;i++){
+            //     pss[i].style.display="none";
+            // }
+            // pss[currIndex].style.display="block";
+        }
+    });
+});
