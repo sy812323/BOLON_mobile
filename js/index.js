efabdb9fd558fa38ef0303e5ffdf5cae7db88f1d
shangyou <317742988@qq.com>
2019-03-02 09:15:53 +0800

    这次提交了首页，注册登录，搜索页面静态布局

---
+++ js/index.js
@@ -0,0 +1,67 @@
+$(function(){
+ 	var mySwiper = new Swiper ('.swiper-container', {
+	    spaceBetween: 30,
+		direction : 'horizontal',
+		centeredSlides: true,
+		autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+		},
+		pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		}
+  	})
+});
+
+$(function(){
+	$(".left_list").toggle(function(){
+		$(".hide_list").css("display","block");
+	},function(){
+		$(".hide_list").css("display","none");
+	});
+	$("#yu").toggle(function(){
+		$("#yu").css({"background":"url(img/arrow_down.png) no-repeat center","background-size":"10px","background-position-x":"92%"});
+		$(".yu_list").css("display","block");
+	},function(){
+		$("#yu").css({"background":"url(img/arrow_up.png) no-repeat center","background-size":"10px","background-position-x":"92%"});
+		$(".yu_list").css("display","none");
+	});
+	$(".sir").toggle(function(){
+			$(".sir").css({"background":"url(img/wap2.png) no-repeat center","background-size":"100%"});
+			$(".sir_list").css("display","block");
+			$(".madam").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".madam_list").css("display","none");
+			$(".brand").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".brand_list").css("display","none");
+		},function(){
+			$(".sir").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".sir_list").css("display","none");
+		}
+	);
+	$(".madam").toggle(function(){
+			$(".madam").css({"background":"url(img/wap2.png) no-repeat center","background-size":"100%"});
+			$(".madam_list").css("display","block");
+			$(".sir").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".sir_list").css("display","none");
+			$(".brand").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".brand_list").css("display","none");
+		},function(){
+			$(".madam").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".madam_list").css("display","none");
+		}
+	);
+	$(".brand").toggle(function(){
+			$(".brand").css({"background":"url(img/wap2.png) no-repeat center","background-size":"100%"});
+			$(".brand_list").css("display","block");
+			$(".sir").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".sir_list").css("display","none");
+			$(".madam").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".madam_list").css("display","none");
+		},function(){
+			$(".brand").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+			$(".brand_list").css("display","none");
+		}
+	);
+
+});
---
+++ js/login.js
@@ -0,0 +1,65 @@
+$(function(){
+    $(".left_list").toggle(function(){
+        $(".hide_list").css("display","block");
+    },function(){
+        $(".hide_list").css("display","none");
+    });
+    $("#yu").toggle(function(){
+        $("#yu").css({"background":"url(img/arrow_down.png) no-repeat center","background-size":"10px","background-position-x":"92%"});
+        $(".yu_list").css("display","block");
+    },function(){
+        $("#yu").css({"background":"url(img/arrow_up.png) no-repeat center","background-size":"10px","background-position-x":"92%"});
+        $(".yu_list").css("display","none");
+    });
+    $(".sir").toggle(function(){
+            $(".sir").css({"background":"url(img/wap2.png) no-repeat center","background-size":"100%"});
+            $(".sir_list").css("display","block");
+            $(".madam").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".madam_list").css("display","none");
+            $(".brand").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".brand_list").css("display","none");
+        },function(){
+            $(".sir").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".sir_list").css("display","none");
+        }
+    );
+    $(".madam").toggle(function(){
+            $(".madam").css({"background":"url(img/wap2.png) no-repeat center","background-size":"100%"});
+            $(".madam_list").css("display","block");
+            $(".sir").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".sir_list").css("display","none");
+            $(".brand").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".brand_list").css("display","none");
+        },function(){
+            $(".madam").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".madam_list").css("display","none");
+        }
+    );
+    $(".brand").toggle(function(){
+            $(".brand").css({"background":"url(img/wap2.png) no-repeat center","background-size":"100%"});
+            $(".brand_list").css("display","block");
+            $(".sir").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".sir_list").css("display","none");
+            $(".madam").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".madam_list").css("display","none");
+        },function(){
+            $(".brand").css({"background":"url(img/wap1.png) no-repeat center","background-size":"100%"});
+            $(".brand_list").css("display","none");
+        }
+    );
+    $(".jizhu a").toggle(function(){
+            $(".jizhu a").addClass("jizhu_dui");
+       },function(){
+            $(".jizhu a").removeClass("jizhu_dui");
+        }
+    );
+    $(".reg").click(function(){
+        $(".login_show").css("display","none");
+        $(".register_show").css("display","block");
+    })
+    $(".forget").click(function(){
+        $(".login_show").css("display","none");
+        $(".register_show").css("display","none");
+        $(".forget_show").css("display","block");
+    })
+});
