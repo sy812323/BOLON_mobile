ee43ce722d6152c80c3f35001103038e7d020792
shangyou <317742988@qq.com>
2019-03-04 21:05:14 +0800

    这次优化了静态购物车的js点击功能

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -48,3 +48,72 @@ $(function(){
         }
     );
 });
+$(function(){
+    $(".list_left a").addClass("return_dui");
+    $(".quanxuan a").addClass("return_dui");
+    $(".right_num_left").click(function(){
+        let count = $(".right_num_center").val();
+        // console.log(count);
+        count--;
+        // console.log(count);
+        if(count<1){
+            count=1;
+        }
+        $(".right_num_center").val(count);
+        if($(".list_left a").hasClass("return_dui")){
+            $(".zongji_rmb span").html(($(".right_num_center").val())*($(".center_rmb span").html()));
+        };
+    });
+    $(".right_num_right").click(function(){
+        let count = $(".right_num_center").val();
+        // console.log(count);
+        count++;
+        // console.log(count);
+        $(".right_num_center").val(count);
+        if($(".list_left a").hasClass("return_dui")){
+            $(".zongji_rmb span").html(($(".right_num_center").val())*($(".center_rmb span").html()));
+        };
+    });
+    $(".list_left a").click(function(){
+        if($(".list_left a").hasClass("return_dui")){
+            $(".list_left a").removeClass("return_dui");
+            $(".quanxuan a").removeClass("return_dui");
+            $(".zongji_rmb span").html("0");
+            $(".pay").css("background-color","#aaa");
+        }else{
+            $(".list_left a").addClass("return_dui");
+            $(".quanxuan a").addClass("return_dui");
+            $(".pay").css("background-color","#000");
+            $(".zongji_rmb span").html(($(".right_num_center").val())*($(".center_rmb span").html()));
+        }
+    });
+    $(".quanxuan a").click(function(){
+        if($(".quanxuan a").hasClass("return_dui")){
+            $(".list_left a").removeClass("return_dui");
+            $(".quanxuan a").removeClass("return_dui");
+            $(".zongji_rmb span").html("0");
+            $(".pay").css("background-color","#aaa");
+        }else{
+            $(".list_left a").addClass("return_dui");
+            $(".quanxuan a").addClass("return_dui");
+            $(".pay").css("background-color","#000");
+            $(".zongji_rmb span").html(($(".right_num_center").val())*($(".center_rmb span").html()));
+        }
+    });
+    $(".delete").click(function(){
+        $(".goods_list,.con_total").css("display","none");
+        $(".m_t_num span").html("0");
+        $(".pay").css("background-color","#aaa");
+        $(".count").html("0");
+        // $(".zongji_rmb span").html(($(".right_num_center").val())*($(".center_rmb span").html()));
+        // $(".quanxuan a").removeClass("return_dui");
+    });
+    $(".right_num_center").blur(function(){
+        if($(".right_num_center").val()==0){
+            $(".right_num_center").val("1");
+            $(".zongji_rmb span").html(($(".right_num_center").val())*($(".center_rmb span").html()));
+        }else{
+            $(".zongji_rmb span").html(($(".right_num_center").val())*($(".center_rmb span").html()));
+        }
+    });
+});
